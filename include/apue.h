3f333022af0d6f65aa15e6c50d8f6152952b309b
Taomercy <taomercy@qq.com>
2018-02-22 20:59:05 +0800

    first

---
+++ include/apue.h
@@ -0,0 +1,67 @@
+/* Our own header, to be included before all standard system headers. */
+#ifndef _APUE_H
+#define _APUE_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#if defined(SOLARIS)      /* Solaris 10  */
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 700
+#endif
+
+#include <sys/types.h>     /* some systems still require this */
+#include <sys/stat.h>
+#include <sys/termios.h>   /* for winsize */
+
+#if defined(MACOS) || !defined(TIOCGWINSZ)
+#include <sys/ioctl.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define MAXLINE 4096      /* max line length */
+
+/* 
+ * Default file access permissions for new files. 
+ */
+#define FILE_MODE  (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+/*
+ * Default permissions for new directories.
+ */
+#define DIR_MODE   (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+
+typedef void  Sigfunc(int);  /* for signal handlers */
+
+#define min(a,b)   ((a) < (b) ? (a) : (b))
+#define max(a,b)   ((a) > (b) ? (a) : (b))
+
+/*
+ * Prototypes for our own functions
+ */
+
+
+
+void err_ret(const char *, ...);      /* Appendix B */
+void err_sys(const char *, ...) __attribute__((noreturn));
+void err_cont(int, const char *, ...);
+void err_exit(int, const char *, ...) __attribute__((noreturn));
+void err_dump(const char *, ...) __attribute__((noreturn));
+void err_msg(const char *, ...);
+void err_quit(const char *, ...) __attribute__((noreturn));
+
+void log_open(const char *, int, int);
+void log_ret(const char *, ...);
+void log_sys(const char *, ...) __attribute__((noreturn));
+void log_msg(const char *, ...);      /* Appendix B */
+void log_quit(const char *, ...) __attribute__((noreturn));
+void log_exit(int, const char *, ...) __attribute__((noreturn));
+
+
+#endif
---
+++ log_error.c
@@ -0,0 +1,131 @@
+/*
+ * Error routines for programs that can run as a daemon.
+ */
+#include "apue.h"
+#include <errno.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+/*
+ * Caller must sefine and set this: nonzero if interactive, zero if daemon
+ */
+int log_to_stderr;
+
+
+static void log_doit(int, int, int, const char *, va_list ap);
+
+
+/*
+ * Initialize syslog(), if running as daemon.
+ */
+void log_open(const char *ident, int option, int facility)
+{
+	if(log_to_stderr == 0)
+		openlog(ident, option, facility);
+}
+
+
+/*
+ * Nonfatal error related to a system call. 
+ * Print a message with the system's errno value and return.
+ */
+void log_ret(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
+}
+
+
+/*
+ * Fatal error related to a system call. 
+ * Print a message and terminate.
+ */
+void log_sys(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
+}
+
+
+/*
+ * Nonfatal error unrelated to a system call. 
+ * Print a message and return.
+ */
+void log_msg(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
+}
+
+
+/*
+ * Fatal error unrelated to a system call. 
+ * Print a message and terminate.
+ */
+void log_quit(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_doit(0, 0, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
+}
+
+
+/*
+ * Fatal error related to a system call. 
+ * Error number passed as an explicit parameter.
+ * Print a message and terminate.
+ */
+void log_exit(int error, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_doit(1, errno, LOG_ERR, fmt, ap);
+	va_end(ap);
+	exit(2);
+}
+
+/*
+ * Print a message and return to caller.
+ * Caller specifies "errorflag" and "priority".
+ */
+static void log_doit(int errnoflag, int error, int priority, const char *fmt, va_list ap)
+{
+	char buf[MAXLINE];
+	vsnprintf(buf, MAXLINE-1, fmt, ap);
+	if(errnoflag)
+		snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, "%s\n",strerror(error));
+	strcat(buf,"\n");
+	if(log_to_stderr)
+	{
+		fflush(stdout);
+		fputs(buf, stderr);
+		fflush(stderr);
+	}
+	else
+	{
+		syslog(priority, "%s", buf);
+	}
+}
+ 
+
+
+
+
+
+
+
+
